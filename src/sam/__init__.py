433332d3f04bd6b4546002a78ab33496a339d48f
Nick DiNapoli <nick.dinapoli@vizgen.com>
2023-12-11 00:17:19 -0500

    SAM on small sample image

---
+++ src/sam/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+MODEL_ROOT = (Path(__file__).parent / "models" / "sam_vit_h_4b8939.pth").resolve()
+DATA_ROOT = (Path(__file__).parent / "data" / "mosaic_DAPI_z0.tif").resolve()
